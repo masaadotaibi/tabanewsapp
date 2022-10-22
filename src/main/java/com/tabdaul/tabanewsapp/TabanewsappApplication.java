55a8b18e2080c37b77c7555b4ebf7322a95c130f
masaadotaibi <masaadbal@gmail.com>
2022-10-23 00:33:20 +0300

    initial commit

---
+++ src/main/java/com/tabdaul/tabanewsapp/TabanewsappApplication.java
@@ -0,0 +1,13 @@
+package com.tabdaul.tabanewsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabanewsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabanewsappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tabdaul/tabanewsapp/TabanewsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tabdaul.tabanewsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabanewsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
